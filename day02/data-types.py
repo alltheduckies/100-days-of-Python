8a3a3ed040a23940a6d71c0ac70e99b905e03824
Eliza <elizawong.126@gmail.com>
2025-05-21 18:10:13 -0400

    Add new folder day02 with data types

---
+++ day02/data-types.py
@@ -0,0 +1,28 @@
+'''
+Primitive data types
+  - string : text
+  - integer : whole numbers
+  - float : decimal numbers
+  - bool : true or false
+  - NoneType : no value or null
+'''
+
+#string
+print(len("hello")) #length of string
+
+print("hello"[2]) #subscripting - pull out the letter from the string, count from 0
+print("hello"[-1])# negative number will select letter backward
+
+#integer (int)
+print(123 + 345)
+
+#large integers
+print(345_567_789) #we usually write big numbers 345,567,789; in python we replace , with _
+
+# float
+print(3.14)
+
+# boolean
+print(true)
+print(false)
+
