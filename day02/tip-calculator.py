8056fec65d39567fc4d5414b4d305d775044655a
Eliza <twewg02@gmail.com>
2025-06-12 15:01:27 -0400

    added greeting for tip calculator

--- day02/tip-calculator.py
+++ day02/tip-calculator.py
@@ -1,4 +1,5 @@
-total_bill = float(input("What was the total bill? "))  #input returns strings, have to convert them to int or float
+print("Welcome to the tip calculator!")
+total_bill = float(input("What was the total bill? $"))  #input returns strings, have to convert them to int or float
 tip = int(input(" How much tip would you like to give? 10, 12, or 15? "))
 number_of_people = int(input("How many people to split the bill? "))
 cost_per_person = (total_bill + (total_bill * (tip / 100)))/ number_of_people
