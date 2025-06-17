9aa4c7a5953fa4c8184a4732dcd3c886f8a34757
Eliza <twewg02@gmail.com>
2025-06-17 09:35:09 -0400

    update comment

--- day03/rollercoaster.py
+++ day03/rollercoaster.py
@@ -5,3 +5,14 @@ if height >= 120:
     print("You can ride the rollercoaster")
 else:
     print("Sorry you are too short!")
+
+
+    # equal sign: "=" is assignment, "==" check equality, "!=" not equal to
+
+
+    '''
+     modulo operator
+     10 % 5 = 0 (no remainder in division)
+
+
+    '''
