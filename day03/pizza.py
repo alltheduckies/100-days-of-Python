67b56558213327e142a4e41a5699fde4bfb087f2
Eliza <twewg02@gmail.com>
2025-06-18 13:43:11 -0400

    create pizza delivery conditions

---
+++ day03/pizza.py
@@ -0,0 +1,39 @@
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M or L: ").upper()
+pepperoni = input("Do you want pepperoni on your pizza? Y or N: ").upper()
+extra_cheese = input("Do you want extra cheese? Y or N: ").upper()
+
+'''
+small pizza (S)= $15
+medium pizza (M) = $20
+large pizza (L) = $25
+Add pepperoni for small pizza (Y or N) : $2
+Add pepperoni for medium or large pizza (Y or N): +$3
+Add extra cheese for any size pizza (Y or N): +1
+'''
+small = 15
+medium = 20
+large = 25
+price = 0
+
+if size == "S":
+    price = small
+elif size == "M":
+   price = medium
+else:
+   price = large
+
+# add pepperoni or not
+
+if pepperoni == "Y":
+    if size == "S":
+        price += 2
+    else:
+        price += 3
+
+# add extra cheese or not
+
+if extra_cheese == "Y":
+    price += 1
+
+print(f"Your final bill is: {price}")
