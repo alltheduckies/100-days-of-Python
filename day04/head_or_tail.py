fac4718c8d4ce09651e44626fcb77729b3d93708
Eliza <twewg02@gmail.com>
2025-06-20 14:09:03 -0400

    add alternative way to generate 0 or 1

--- day04/head_or_tail.py
+++ day04/head_or_tail.py
@@ -4,7 +4,15 @@ import random
 number_0_or_1 =round(random.random())
 
 if number_0_or_1 == 0:
-    print("Tail")
-else:
     print("Head")
+else:
+    print("Tail")
 
+# or use randint()
+
+random_int_number = random.randint(0, 1)
+
+if random_int_number == 0:
+    print("Heads")
+else:
+    print("Tails")
