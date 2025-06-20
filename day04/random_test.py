c3440be928338bf9ef8205943dd0b11b913f3028
Eliza <twewg02@gmail.com>
2025-06-20 13:58:28 -0400

    generate random numbers

---
+++ day04/random_test.py
@@ -0,0 +1,13 @@
+import random
+
+random_number_0_to_1  = random.random()
+
+print(round(random_number_0_to_1, 2))
+
+random_number_1_to_10  = random.random() * 10
+
+print(round(random_number_1_to_10, 2))
+
+random_float = random.uniform(5,7)
+
+print(random_float)
